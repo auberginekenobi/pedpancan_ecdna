e26c8bc41661a4ec7e1d5fc8fa4dbcace71ae496
rgoto0516-star <r.goto0516@gmail.com>
2025-11-16 22:02:23 -0800

    Somatic variants updates

---
+++ 2025-10-06_somatic-variants/bcftools_stats_likely_pathogenic.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Using bcftools, iterate through all .vcf.gz files,
+# and write the bcftools stats outputs to a new file.
+
+INPUT_DIR="./data/filter/likely_pathogenic"
+OUTPUT_DIR="./data/stats/likely_pathogenic"
+
+for file in $INPUT_DIR/*.vcf.gz; do
+	OUT_FILE=$(basename $file .consensus_somatic.norm.annot.public.filtered.vcf.gz)
+	OUT_FILE=$OUTPUT_DIR/${OUT_FILE}.stats.txt
+	echo "Processing $OUT_FILE ..."
+	bcftools stats -f PASS,. $file > $OUT_FILE
+done
