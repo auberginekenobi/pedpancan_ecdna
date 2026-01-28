4bbca8d890de3cbe6ae709b1ddad06397aee7f30
Owen Chapman <owenchapman1@gmail.com>
2026-01-28 16:26:20 +0900

    primary secondary barplot - better colors

--- src/seaborn_helper_functions.py
+++ src/seaborn_helper_functions.py
@@ -1,4 +1,5 @@
 import seaborn as sns
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 
 # Some helper functions
@@ -9,17 +10,16 @@ def savefig(fig, basename,dpi=300):
     fig.savefig(svgName,format='svg')
 def set_plot_defaults(linewidth=0.75,fontsize=7):
     sns.set_style("white")
-    sns.set(rc={'svg.fonttype':'none',
-                'pdf.use14corefonts':True,
-                'figure.figsize':(7,2),
-                'font.size': fontsize,'axes.labelsize': fontsize,'axes.titlesize': fontsize,'xtick.labelsize': fontsize,
-                'ytick.labelsize': fontsize,'legend.fontsize': fontsize,
-                'font.family':'sans-serif',
-                'font.sans-serif':'Arial',
-                'axes.linewidth':linewidth,'ytick.major.width':linewidth,'xtick.major.width':linewidth,
-                'patch.force_edgecolor':False,
-                'ytick.left':True,
-                })
-    plt.tight_layout()
-    sns.despine()
+    mpl.rcParams.update({
+        'svg.fonttype':'none',
+        'pdf.use14corefonts':True,
+        'figure.figsize':(7,2),
+        'font.size': fontsize,'axes.labelsize': fontsize,'axes.titlesize': fontsize,'xtick.labelsize': fontsize,
+        'ytick.labelsize': fontsize,'legend.fontsize': fontsize,
+        'font.family':'sans-serif',
+        'font.sans-serif':'Arial',
+        'axes.linewidth':linewidth,'ytick.major.width':linewidth,'xtick.major.width':linewidth,
+        'patch.force_edgecolor':False,
+        'ytick.left':True,
+    })
     return
