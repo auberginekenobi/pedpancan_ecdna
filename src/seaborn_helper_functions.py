21855796196fd9a6ae7df82f1e9418520de1ea5c
Owen Chapman <owenchapman1@gmail.com>
2026-02-18 15:14:13 +0900

    finalize oncogene cn analysis; refactor.

--- src/seaborn_helper_functions.py
+++ src/seaborn_helper_functions.py
@@ -8,17 +8,19 @@ def savefig(fig, basename,dpi=300):
     svgName = basename + ".svg"
     fig.savefig(pngName,format='png', dpi=dpi, bbox_inches='tight')
     fig.savefig(svgName,format='svg')
-def set_plot_defaults(linewidth=0.75,fontsize=7):
+def set_plot_defaults(linewidth=0.75,fontsize=7,figsize=(7,2)):
     sns.set_style("white")
     mpl.rcParams.update({
         'svg.fonttype':'none',
         'pdf.use14corefonts':True,
-        'figure.figsize':(7,2),
+        'figure.figsize':figsize,
         'font.size': fontsize,'axes.labelsize': fontsize,'axes.titlesize': fontsize,'xtick.labelsize': fontsize,
         'ytick.labelsize': fontsize,'legend.fontsize': fontsize,
         'font.family':'sans-serif',
         'font.sans-serif':'Arial',
-        'axes.linewidth':linewidth,'ytick.major.width':linewidth,'xtick.major.width':linewidth,
+        'axes.linewidth':linewidth,'ytick.major.width':linewidth,'ytick.minor.width':linewidth,'xtick.major.width':linewidth,
+        'xtick.minor.width':linewidth,
+        'xtick.major.size':2,
         'patch.force_edgecolor':False,
         'ytick.left':True,
     })
