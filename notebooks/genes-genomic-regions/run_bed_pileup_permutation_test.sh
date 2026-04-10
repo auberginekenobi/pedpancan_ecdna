f33917884d28dae89679804cbb20fac56e211542
Owen Chapman <owenchapman1@gmail.com>
2026-04-10 15:01:07 +0900

    genomic regions analyses cleanup

--- notebooks/genes-genomic-regions/run_bed_pileup_permutation_test.sh
+++ notebooks/genes-genomic-regions/run_bed_pileup_permutation_test.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
+## Run permutation test to evaluate statistical likelihood of independent overlapping ecDNA regions.
+## See also src/bed_pileup_permutation_test.sh
 
 # Define inputs
-AA_DATA_REPO=$HOME/anno/AmpliconArchitect/GRCh38
-genome=$HOME/anno/genomes/human.hg38.noalt.genome
+PROJ_ROOT=../..
+AA_DATA_REPO=$PROJ_ROOT/anno/AmpliconArchitect/GRCh38
+genome=$PROJ_ROOT/anno/genomes/human.hg38.noalt.genome
 blacklist=$AA_DATA_REPO/GRCh38_merged_centromeres_conserved_sorted.bed
 bed_dir=$(pwd)/bed_symlinks/ecDNA_all
 
 # Run
-perm_test="../src/bed_pileup_permutation_test.sh"
+perm_test="../../src/bed_pileup_permutation_test.sh"
 $perm_test -i $bed_dir -g $genome -b $blacklist -o "tmp/ecDNA"
 
 bed_dir=$(pwd)/bed_symlinks/intrachromosomal_all
